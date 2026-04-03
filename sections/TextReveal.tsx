04deae3dc1610e135a9cb7871d71e1e535c44bc5
Abhay Maurya <abhaymaurya821@gmail.com>
2026-04-03 17:44:18 +0530

    feat: added the text reveal animations

--- sections/TextReveal.tsx
+++ sections/TextReveal.tsx
@@ -1,15 +1,76 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function TextReveal() {
+  const fillRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo(
+      fillRef.current,
+      { width: "0%" },
+      {
+        width: "100%",
+        ease: "none",
+        scrollTrigger: {
+          trigger: fillRef.current,
+          start: "top 80%",
+          end: "top 20%",
+          scrub: 1,
+          markers:true,
+        },
+      }
+    );
+  }, []);
+
   return (
-    <section className="min-h-screen pt-20 ">
-      <div className="flex justify-center items-center">
-        <div>
-            <img src="/images.jpeg" alt="features_1" />
+    <section className="min-h-screen py-16">
+      <div className="max-w-7xl mx-auto px-4 grid gap-12 lg:grid-cols-2 items-center">
+        
+        {/* LEFT: Image */}
+        <div className="flex justify-center">
+          <img
+            src="/images.jpeg"
+            alt="features"
+            className="w-70 sm:w-87.5 md:w-105 rounded-2xl shadow-xl"
+          />
         </div>
-        <div>
-          <h1>Text-Reveal animations</h1>
+
+        {/* RIGHT: Text */}
+        <div className="space-y-6 text-center lg:text-left">
+          
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
+            Home
+          </h1>
+
+          {/* Text Reveal */}
+          <div className="relative">
+            
+            {/* BACK TEXT */}
+            <p className="text-gray-400/30 text-sm sm:text-base md:text-lg leading-relaxed">
+              With a bold clock, encouraging quote, and personalized greetings
+              that evolve with your day, the Home dashboard syncs with your
+              daily rhythm.
+            </p>
+
+            {/* FRONT TEXT */}
+            <p
+              ref={fillRef}
+              className="absolute inset-0 text-white overflow-hidden whitespace-nowrap"
+              style={{ width: "0%" }}
+            >
+              With a bold clock, encouraging quote, and personalized greetings
+              that evolve with your day, the Home dashboard syncs with your
+              daily rhythm.
+            </p>
+
+          </div>
         </div>
+
       </div>
     </section>
   );
