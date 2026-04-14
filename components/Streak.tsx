a4eef4ab195e0e3faa1b4d40248c624a4ac6c8a3
Abhay Maurya <abhaymaurya821@gmail.com>
2026-04-14 17:55:28 +0530

    feat: Proper indentation and added the dashboard redirect

--- components/Streak.tsx
+++ components/Streak.tsx
@@ -2,11 +2,10 @@
 
 import { useEffect, useState } from "react";
 import { calculateStreak } from "../hooks/streak";
-import { Maximize, Minimize } from "lucide-react";
+
 
 export default function Streak() {
   const [streak, setStreak] = useState(0);
-  const [isFullscreen, setIsFullscreen] = useState(false);
 
   useEffect(() => {
     const fetchSessions = async () => {
@@ -23,33 +22,11 @@ export default function Streak() {
   return (
     <>
       <section className="flex justify-center items-center">
-        <div className="bg-black text-white p-6 rounded-xl text-center">
+        <div className=" text-white rounded-xl text-center">
           <h2 className="text-xl font-semibold">🔥 {streak}</h2>
         </div>
 
-        <div>
-          {!isFullscreen ? (
-            <button
-              onClick={() => {
-                document.documentElement.requestFullscreen();
-                setIsFullscreen(true);
-              }}
-              className="border px-4 py-2"
-            >
-              <Maximize />
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                document.exitFullscreen();
-                setIsFullscreen(false);
-              }}
-              className="border px-4 py-2"
-            >
-              <Minimize />
-            </button>
-          )}
-        </div>
+      
       </section>
     </>
   );
--- components/Timer.tsx
+++ components/Timer.tsx
@@ -3,7 +3,14 @@
 import { useTimer } from "@/hooks/useTimer";
 import { useState, useEffect } from "react";
 import Streak from "./Streak";
-import { PictureInPicture2, TimerReset } from "lucide-react";
+import { useRouter } from "next/navigation";
+import {
+  PictureInPicture2,
+  TimerReset,
+  Maximize,
+  Minimize,
+  LayoutDashboard,
+} from "lucide-react";
 
 
 const MODES = {
@@ -14,13 +21,15 @@ const MODES = {
 
 export default function Timer() {
   const { time, setTime, start, stop, reset, running } = useTimer();
+  const router = useRouter();
 
   const [mode, setMode] = useState<"FOCUS" | "SHORT_BREAK" | "LONG_BREAK">(
     "FOCUS",
   );
 
   const [mounted, setMounted] = useState(false);
-  const [hidden, setHidden] = useState(false); // ✅ NEW
+  const [hidden, setHidden] = useState(false);
+  const [isFullscreen, setIsFullscreen] = useState(false);
 
   useEffect(() => {
     setMounted(true);
@@ -52,86 +61,125 @@ export default function Timer() {
       "width=400,height=300,top=200,left=800",
     );
 
-    setHidden(true); // ✅ hide background timer
+    setHidden(true); //  hide background timer
   };
-    
 
-  
-  // 👇 If hidden, show minimal UI instead
+  // If hidden, show minimal UI instead
   if (hidden) {
-    return (
-      <div></div>
-    );
+    return <div></div>;
   }
-  
- 
+
   return (
-    <div className="flex flex-col gap-4 justify-center items-center min-h-screen">
-      {/* Mode selection */}
-      <div className="flex gap-2">
-        <button
-          onClick={() => {
-            setMode("FOCUS");
-            setTime(MODES.FOCUS);
-          }}
-          className="bg-blue-500 px-3 py-1 text-white"
-        >
-          Focus
-        </button>
+    <>
+      <div className="flex flex-col gap-4 justify-center items-center min-h-screen">
+        {/* Mode selection */}
+        <div className="flex gap-2">
+          <button
+            onClick={() => {
+              setMode("FOCUS");
+              setTime(MODES.FOCUS);
+            }}
+            className="bg-blue-500 px-3 py-1 text-white"
+          >
+            Focus
+          </button>
 
-        <button
-          onClick={() => {
-            setMode("SHORT_BREAK");
-            setTime(MODES.SHORT_BREAK);
-          }}
-          className="bg-purple-500 px-3 py-1 text-white"
-        >
-          Break
-        </button>
+          <button
+            onClick={() => {
+              setMode("SHORT_BREAK");
+              setTime(MODES.SHORT_BREAK);
+            }}
+            className="bg-purple-500 px-3 py-1 text-white"
+          >
+            Break
+          </button>
 
-        <button
-          onClick={() => {
-            setMode("LONG_BREAK");
-            setTime(MODES.LONG_BREAK);
-          }}
-          className="bg-yellow-500 px-3 py-1 text-white"
-        >
-          Long Break
-        </button>
+          <button
+            onClick={() => {
+              setMode("LONG_BREAK");
+              setTime(MODES.LONG_BREAK);
+            }}
+            className="bg-yellow-500 px-3 py-1 text-white"
+          >
+            Long Break
+          </button>
+        </div>
+
+        {/* Timer */}
+        <div className="text-4xl font-bold lg:text-9xl md:text-7xl sm:text-5xl">
+          {Math.floor(time / 60)}:{String(time % 60).padStart(2, "0")}
+        </div>
+
+        <div className="flex justify-center items-center gap-3 ">
+          {/* Controls */}
+          {!running ? (
+            <button
+              onClick={start}
+              className="bg-purple-500 px-4 py-2 text-white cursor-pointer"
+            >
+              Start
+            </button>
+          ) : (
+            <button
+              onClick={stop}
+              className="bg-purple-900 px-4 py-2 text-white cursor-pointer"
+            >
+              Stop
+            </button>
+          )}
+          <button
+            onClick={handleReset}
+            className="px-4 py-2 text-white cursor-pointer"
+          >
+            <TimerReset />
+          </button>
+
+          {/* 🔥 POPUP BUTTON */}
+          <button
+            onClick={openPopup}
+            className=" px-4 py-2 text-white rounded cursor-pointer"
+          >
+            <PictureInPicture2 />
+          </button>
+        </div>
+
+        <div className="flex justify-center items-center gap-3 ">
+          {/* streak */}
+          <Streak />
+          {/* Full screen and default screen */}
+          {!isFullscreen ? (
+            <button
+              onClick={() => {
+                document.documentElement.requestFullscreen();
+                setIsFullscreen(true);
+              }}
+              className="border px-4 py-2 cursor-pointer"
+            >
+              <Maximize />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                document.exitFullscreen();
+                setIsFullscreen(false);
+              }}
+              className="border px-4 py-2"
+            >
+              <Minimize />
+            </button>
+          )}
+
+          {/* dashboard */}
+          <div>
+            <button
+              onClick={() => router.push("/dashboard")}
+              className="px-4 py-2 text-white rounded"
+            >
+              <LayoutDashboard />
+            </button>
+          </div>
+        </div>
       </div>
-
-      {/* Timer */}
-      <div className="text-4xl font-bold">
-        {Math.floor(time / 60)}:{String(time % 60).padStart(2, "0")}
-      </div>
-
-      {/* Controls */}
-      {!running ? (
-        <button onClick={start} className="bg-green-500 px-4 py-2 text-white">
-          Start
-        </button>
-      ) : (
-        <button onClick={stop} className="bg-red-500 px-4 py-2 text-white">
-          Stop
-        </button>
-      )}
-
-      <button
-        onClick={handleReset}
-        className="bg-gray-600 px-4 py-2 text-white"
-      >
-        <TimerReset />
-      </button>
-
-      {/* 🔥 POPUP BUTTON */}
-      <button
-        onClick={openPopup}
-        className="bg-indigo-500 px-4 py-2 text-white rounded"
-      >
-        <PictureInPicture2 />
-      </button>
-
-      <Streak />
-    </div>
+    </>
   );
 }
